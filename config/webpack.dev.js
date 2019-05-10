0d24c0f3a863ee888ce449117c1c47dd5db7b087
Tan Jin Jie <453089136@qq.com>
2019-05-10 18:42:57 +0800

    增加了babel的缓存编译，多线程编译

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,11 +1,11 @@
 const { resolve } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
-const WorkboxPlugin = require('workbox-webpack-plugin')
+const os = require('os')
 module.exports = {
     entry: {
         app: ['./src/index.js', './src/index.html'],
-        vendor: ['react', ]
+        vendor: ['react',]
     },
     output: {
         filename: '[name].[hash:8].js',
@@ -14,59 +14,68 @@ module.exports = {
     module: {
         rules: [
             {
-                enforce:'pre',
-                test:/\.js$/,
-                exclude:/node_modules/,
-                include:resolve(__dirname,'/src/js'),
-                loader:'eslint-loader'
+                enforce: 'pre',
+                test: /\.js$/,
+                exclude: /node_modules/,
+                include: resolve(__dirname, '/src/js'),
+                loader: 'eslint-loader'
+            },
+            {
+                oneOf: [{
+                    test: /\.(html)$/,
+                    loader: 'html-loader'
                 },
-            {
-            oneOf: [{
-                test: /\.(html)$/,
-                loader: 'html-loader'
-            },
-            {
-                test: /\.(js|jsx)$/,
-                use:
                 {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ["@babel/preset-react", ["@babel/preset-env", { "modules": false }]],
-                        plugins: ["@babel/plugin-syntax-dynamic-import"]
-                    },
+                    test: /\.(js|jsx)$/,
+                    use: [
+                        {
+                            loader: 'thread-loader',
+                            options: {
+                                workers: os.cpus().length
+                            }
+                        },
+                        {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: ["@babel/preset-react", ["@babel/preset-env", { "modules": false }]],
+                                plugins: ["@babel/plugin-syntax-dynamic-import"],
+                                cacheDirectory: true
+                            },
 
-                }
-            },
-            {
-                test: /\.(less)$/,
-                use: [
-                    { loader: 'style-loader' },
-                    {
-                        loader: 'css-loader', options: {
-                            modules: true,
-                            localIdentName: '[local]--[hash:base64:5]'
                         }
-                    },
-                    { loader: 'less-loader' }
+                    ]
+
+                },
+                {
+                    test: /\.(less)$/,
+                    use: [
+                        { loader: 'style-loader' },
+                        {
+                            loader: 'css-loader', options: {
+                                modules: true,
+                                localIdentName: '[local]--[hash:base64:5]'
+                            }
+                        },
+                        { loader: 'less-loader' }
+                    ]
+                }, {
+                    test: /\.(jpg|jpeg|bmp|svg|png|webp|gif)$/,
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8 * 1024,
+                        name: '[name].[hash:8].[ext]',
+
+                    }
+                }, {
+                    exclude: /\.(js|json|less|css|jsx)$/,
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'media/',
+                        name: '[name].[hash].[ext]'
+                    }
+                }
                 ]
-            }, {
-                test: /\.(jpg|jpeg|bmp|svg|png|webp|gif)$/,
-                loader: 'url-loader',
-                options: {
-                    limit: 8 * 1024,
-                    name: '[name].[hash:8].[ext]',
-                    
-                }
-            }, {
-                exclude: /\.(js|json|less|css|jsx)$/,
-                loader: 'file-loader',
-                options: {
-                    outputPath: 'media/',
-                    name: '[name].[hash].[ext]'
-                }
-            }
-            ]
-        }]
+            }]
     },
     plugins: [
         new HtmlWebpackPlugin({
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,14 +1,15 @@
-const { resolve} = require('path')
+const { resolve } = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 const WorkboxPlugin = require('workbox-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const os = require('os')
 module.exports = {
     entry: {
         app: ['./src/index.js', './src/index.html'],
-        vendor: ['react','react-redux','redux','better-scroll'] 
+        vendor: ['react']
     },
     output: {
         filename: '[name].[contenthash:8].js',
@@ -17,121 +18,132 @@ module.exports = {
     module: {
         rules: [
             {
-            enforce:'pre',
-            test:/\.js$/,
-            exclude:/node_modules/,
-            include:resolve(__dirname,'/src/js'),
-            loader:'eslint-loader'
+                enforce: 'pre',
+                test: /\.js$/,
+                exclude: /node_modules/,
+                include: resolve(__dirname, '/src/js'),
+                loader: 'eslint-loader'
             },
             {
-            oneOf: [{
-                test: /\.(html)$/,
-                loader: 'html-loader'
-            },
-            {
-                test: /\.(js|jsx)$/,
-                use:
+                oneOf: [{
+                    test: /\.(html)$/,
+                    loader: 'html-loader'
+                },
                 {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ["@babel/preset-react", ["@babel/preset-env", { "modules": false }]],
-                        plugins: ["@babel/plugin-syntax-dynamic-import"]
-                    },
+                    test: /\.(js|jsx)$/,
+                    use: [
+                        {
+                            loader: 'thread-loader',
+                            options: {
+                                workers: os.cpus().length
+                            }
+                        }, {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: ["@babel/preset-react", ["@babel/preset-env", { "modules": false }]],
+                                plugins: ["@babel/plugin-syntax-dynamic-import"],
+                                cacheDirectory: true
+                            },
 
-                }
-            },
-            {
-                test: /\.(less)$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader', options: {
-                            modules: true,
-                            localIdentName: '[local]--[hash:base64:5]'
                         }
-                    },
-                    {loader:'postcss-loader'},
-                    { loader: 'less-loader' }
-                ]
-            }, {
-                test: /\.(jpg|jpeg|bmp|svg|png|webp|gif)$/,
-                
-                use:[
-                    {loader: 'url-loader',
-                    options: {
-                        limit: 8 * 1024,
-                        name: '[name].[hash:8].[ext]',
-                        outputPath:'/img'
-                    }},
-                    {
-                        loader: 'img-loader',
-                        options: {
-                          plugins: [
-                            require('imagemin-gifsicle')({
-                              interlaced: false
-                            }),
-                            require('imagemin-mozjpeg')({
-                              progressive: true,
-                              arithmetic: false
-                            }),
-                            require('imagemin-pngquant')({
-                              floyd: 0.5,
-                              speed: 2
-                            }),
-                            require('imagemin-svgo')({
-                              plugins: [
-                                { removeTitle: true },
-                                { convertPathData: false }
-                              ]
-                            })
-                          ]
-                        }
-                      }
-                ]
-                
-                
+                    ]
 
-            }, {
-                exclude: /\.(js|json|less|css|jsx)$/,
-                loader: 'file-loader',
-                options: {
-                    outputPath: 'media/',
-                    name: '[name].[contenthash:8].[ext]'
+                },
+                {
+                    test: /\.(less)$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader', options: {
+                                modules: true,
+                                localIdentName: '[local]--[hash:base64:5]'
+                            }
+                        },
+                        { loader: 'postcss-loader' },
+                        { loader: 'less-loader' }
+                    ]
+                },
+                {
+                    test: /\.(jpg|jpeg|bmp|svg|png|webp|gif)$/,
+
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                limit: 8 * 1024,
+                                name: '[name].[hash:8].[ext]',
+                                outputPath: '/img'
+                            }
+                        },
+                        {
+                            loader: 'img-loader',
+                            options: {
+                                plugins: [
+                                    require('imagemin-gifsicle')({
+                                        interlaced: false
+                                    }),
+                                    require('imagemin-mozjpeg')({
+                                        progressive: true,
+                                        arithmetic: false
+                                    }),
+                                    require('imagemin-pngquant')({
+                                        floyd: 0.5,
+                                        speed: 2
+                                    }),
+                                    require('imagemin-svgo')({
+                                        plugins: [
+                                            { removeTitle: true },
+                                            { convertPathData: false }
+                                        ]
+                                    })
+                                ]
+                            }
+                        }
+                    ]
+
+
+
+                }, {
+                    exclude: /\.(js|json|less|css|jsx)$/,
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'media/',
+                        name: '[name].[contenthash:8].[ext]'
+                    }
                 }
-            }
-            ] 
-        }]
+                ]
+            }]
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
             minify: {
-                removeComments: true,  
-                collapseWhitespace: true,  
+                removeComments: true,
+                collapseWhitespace: true,
                 removeRedundantAttributes: true,
-                useShortDoctype: true, 
+                useShortDoctype: true,
                 removeEmptyAttributes: true,
                 removeStyleLinkTypeAttributes: true,
-                keepClosingSlash: true, 
+                keepClosingSlash: true,
                 minifyJS: true,
-                minifyCSS: true, 
-                minifyURLs: true, 
-              }
+                minifyCSS: true,
+                minifyURLs: true,
+            }
         }),
         new webpack.NamedModulesPlugin(),
         new WorkboxPlugin.GenerateSW({
             clientsClaim: true,
             skipWaiting: true,
             importWorkboxFrom: 'local',
-            include: [/\.js$/, /\.css$/, /\.html$/,/\.jpg/,/\.jpeg/,/\.svg/,/\.webp/,/\.png/],
+            include: [/\.js$/, /\.css$/, /\.html$/, /\.jpg/, /\.jpeg/, /\.svg/, /\.webp/, /\.png/],
         }),
         new MiniCssExtractPlugin({
-            filename:'[name].[contenthash:8].css'
+            filename: '[name].[contenthash:8].css'
         }),
         new CleanWebpackPlugin(),
         new OptimizeCssAssetsWebpackPlugin({
-            cssProcessPluginOptions:{
-                preset:['default',{discardComments: {removeAll:true} }]
+            cssProcessPluginOptions: {
+                preset: ['default', { discardComments: { removeAll: true } }]
             }
         }),
         new webpack.HashedModuleIdsPlugin()
@@ -141,7 +153,7 @@ module.exports = {
         extensions: [".js", ".json", ".jsx"]
     },
     optimization: {
-        runtimeChunk:true,
+        runtimeChunk: true,
         splitChunks: {
             chunks: 'all'
         }
