2514cde49dea0db840ff8c46473d381b1a24cd51
tanjinjie <a@PeterdeMacBook-Pro.local>
2019-10-13 22:34:05 +0800

    完成修复

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
-const app = require('express')();
-
+const express = require('express');
+const app = express();
+const path = require('path');
 app.use(express.static(path.join(__dirname, '../dist')));
 
 app.listen(3000, err => {
